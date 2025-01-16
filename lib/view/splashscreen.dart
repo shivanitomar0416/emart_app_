5e88485e681affa096d2b2533c88165760594462
shivanitomarofficial@gmail.com <shivanitomarofficial@gmail.com>
2025-01-16 22:12:25 +0530

    8TH

--- lib/view/splashscreen.dart
+++ lib/view/splashscreen.dart
@@ -1,20 +1,29 @@
 import 'package:emart_app_/consts/consts.dart';
-
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
-
   @override
   _SplashScreenState createState() => _SplashScreenState();
 }
 class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+ return Scaffold(
       backgroundColor: Vx.orange300,
      body: Center (
       child: Column(
         children: [
-            Align(alignment: Alignment.topLeft, child: Image.asset(icSplashBg, width: 300,)),
+            Align(alignment: Alignment.topLeft, 
+            child: Image.asset(icSplashBg, width: 300,)
+            ),
+            20.heightBox,
+            applogoWidget(),
+            10.heightBox,
+            appname.text.fontFamily(bold).size(22).white.make(),
+            5.heightBox,
+            appversion.text.white.make(),
+            const Spacer(),
+            credits.text.white.fontFamily(semibold).make(),
+            30.heightBox,
         ],
             ),
         ),
